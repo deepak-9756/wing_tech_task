e83dd7a3b1ed53b1a5fafab1bed14e1b01c8ea07
deepak <deepaksharma64349.a@gmail.com>
2025-09-22 23:08:43 +0530

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,73 +1,28 @@
-// lib/main.dart
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:firebase_auth/firebase_auth.dart';
-import 'package:get/get.dart';
-import 'screens/dashboard_screen.dart';
-import 'screens/login_screen.dart';
-import 'controllers/auth_controller.dart';
 
-void main() async {
+import 'package:wing_tech_task/widgets_tree.dart';
+ 
+ 
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  
-  // Firebase initialization
   await Firebase.initializeApp();
-  
-  // Initialize GetX controllers
-  Get.put(AuthController());
-  
-  runApp(AssetManagementApp());
+  runApp(const MyApp());
 }
 
-class AssetManagementApp extends StatelessWidget {
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key : key);
+
   @override
   Widget build(BuildContext context) {
-    return GetMaterialApp(
-      title: 'Asset Management System',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-        appBarTheme: AppBarTheme(
-          backgroundColor: Colors.blue[700],
-          foregroundColor: Colors.white,
-          elevation: 2,
-        ),
-        cardTheme: CardTheme(
-          elevation: 4,
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(8),
-          ),
-        ),
-      ),
-      home: AuthWrapper(),
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
-    );
-  }
-}
+      theme: ThemeData(
+        primarySwatch: Colors.red,
 
-class AuthWrapper extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return StreamBuilder<User?>(
-      stream: FirebaseAuth.instance.authStateChanges(),
-      builder: (context, snapshot) {
-        // Show loading while checking auth state
-        if (snapshot.connectionState == ConnectionState.waiting) {
-          return Scaffold(
-            body: Center(
-              child: CircularProgressIndicator(),
-            ),
-          );
-        }
-        
-        // User is logged in
-        if (snapshot.hasData && snapshot.data != null) {
-          return DashboardScreen();
-        }
-        
-        // User is not logged in
-        return LoginScreen();
-      },
+      ),
+      home: const WidgetTree(),
     );
   }
+
 }
---
+++ lib/widgets_tree.dart
@@ -0,0 +1,35 @@
+ 
+import 'package:wing_tech_task/pages/home_page.dart';
+import 'package:wing_tech_task/pages/login.dart';
+
+import 'auth.dart';
+
+import 'package:flutter/material.dart';
+
+
+class WidgetTree extends StatefulWidget{
+  const WidgetTree ({Key? key}) : super(key : key);
+
+  @override
+  State<WidgetTree> createState()=> _WidgetTreeState();
+  
+  
+  
+}
+
+class _WidgetTreeState extends State<WidgetTree>{
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder(
+        stream: Auth().authStatechanges,
+        builder: (context,snapshot){
+          if(snapshot.hasData){
+            return HomePage();
+          }else {
+            return const LoginPage();
+          }
+        },
+    );
+  }
+  
+}
